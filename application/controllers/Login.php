94b51680397177e2de8440653cdf7eed3b5d5086
FalahRafif <frthegamer95@gmail.com>
2021-08-09 10:24:09 +0700

    membuat method untuk mengupdate data akun guru,siswa,admin

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -30,7 +30,60 @@ class Login extends CI_Controller {
         $data = '';
         
         $this->load->view('login/login', $data);
+        $plain_text = 'This is a plain-text message!';
+        $ciphertext = $this->encryption->encrypt($plain_text);
 
+        // Outputs: This is a plain-text message!
+        echo $this->encryption->decrypt($ciphertext);
+        var_dump($ciphertext);
+
+    }
+    public function updating_data()
+    {
+        echo 'Updating data.....';
+        //// get all data user
+        $siswa = $this->model_login->getAllDataUser('siswa');
+        $guru = $this->model_login->getAllDataUser('guru');
+        $admin = $this->model_login->getAllDataUser('admin');
+        // count all data
+        $countData = $siswa[0] + $guru[0] + $admin[0];
+        $countDataCek = $siswa[0] + $guru[0] + $admin[0];
+        /// update password all akun
+        // siswa 
+        for ($i = $siswa[0] - 1; $i != -1; $i--) { 
+            $siswa[1][$i]['pw'] = $this->encryption->encrypt($siswa[1][$i]['pw']);
+            $countData = $countData - 1;
+        }
+        // guru
+        for ($i = $guru[0] - 1; $i != -1; $i--) { 
+            $guru[1][$i]['pw'] = $this->encryption->encrypt($guru[1][$i]['pw']);
+            $countData = $countData - 1;
+        }
+        // admin
+        for ($i = $admin[0] - 1; $i != -1; $i--) { 
+            $admin[1][$i]['pw'] = $this->encryption->encrypt($admin[1][$i]['pw']);
+            $countData = $countData - 1;
+        }
+        /// cek data error 
+        if($countData != $countDataCek){
+            $countDataError = $countData;
+        }
+        /// insert data
+        $cek['siswa'] = $this->model_login->insertDatasNew($siswa, 'akun_siswa', 'id_akun_siswa');
+        $cek['guru'] = $this->model_login->insertDatasNew($guru, 'akun_guru', 'id_akun_guru');
+        $cek['admin'] = $this->model_login->insertDatasNew($admin, 'akun_admin', 'id_admin');
+        /// result method 
+        /// error while encryption 
+        echo '
+            <h1> Result Updating Data </h1>
+            <hr>
+            <h3>Error While Encryption :</h3>
+            <p>' . $countDataError . ' <p>
+            <h3>Error While Updating :</h3>
+            <p> Siswa : ' . $cek['siswa'] . ' <p>
+            <p> Guru : ' . $cek['guru'] . ' <p>
+            <p> Admin : ' . $cek['admin'] . ' <p>
+        ';
     }
     public function login()
     {
